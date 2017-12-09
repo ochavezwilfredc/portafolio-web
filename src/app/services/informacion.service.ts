eb13f8bb71a40a1a5e888f4b1a6baf629626649f
Cristobal olano <ochavezwilfredc@crece.uss.edu.pe>
2017-12-09 00:58:48 -0500

    Servicio de firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,16 +5,35 @@ import { Http } from '@angular/http';
 export class InformacionService {
   info: any = {};
   cargada: boolean = false;
+  cargada_sobre_nosotros: boolean = false;
+  equipo: any = [];
 
   constructor(public http: Http) {
+        this.carga_info();
+        this.carga_sobre_nosotros();
+
+  }
+
+  public carga_info() {
     this.http.get("assets/data/info.pagina.json").subscribe(
       data => {
-        console.log(data.json());
+        //console.log(data.json());
         this.info = data.json();
         this.cargada = true;
       }
     )
-
   }
+
+  public carga_sobre_nosotros() {
+    this.http.get("https://webangular-c5058.firebaseio.com/equipo.json").subscribe(
+      data => {
+        console.log(data.json());
+        this.equipo = data.json();
+        this.cargada_sobre_nosotros = true;
+      }
+    )
+  }
+
+
 
 }
