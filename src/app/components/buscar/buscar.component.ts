c3aee3ea70991919ab0fde6ba3d3355dc00be811
Cristobal olano <ochavezwilfredc@crece.uss.edu.pe>
2017-12-09 21:19:38 -0500

    primera version finalizada

--- src/app/components/buscar/buscar.component.ts
+++ src/app/components/buscar/buscar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
 
 @Component({
   selector: 'app-buscar',
@@ -7,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarComponent implements OnInit {
 
-  constructor() { }
+  termino: any = undefined;
+  constructor(private route:ActivatedRoute,
+              private _ps:ProductosService) {
+    route.params.subscribe( parametros =>{
+        this.termino = parametros['termino'];
+        console.log(this.termino);
+        _ps.buscar_producto(this.termino);
+    });
+  }
 
   ngOnInit() {
   }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -5,27 +5,63 @@ import { Http } from '@angular/http';
 @Injectable()
 export class ProductosService {
   productos: any = [];
-  cargando :boolean = true;
-  constructor(public http:Http) {
+  productos_filtrados: any = [];
+  cargando: boolean = true;
+  constructor(public http: Http) {
     this.cargar_productos();
   }
 
-  public cargar_productos(){
+  public cargar_productos() {
+    let promesa = new Promise((resolve, reject) => {
+      this.http.get('https://webangular-c5058.firebaseio.com/productos_idx.json').subscribe(
+        res => {
+          // setTimeout(()=>{
+          // console.log(res.json());
+          this.productos = res.json();
+          this.cargando = false;
+          // },1500)
+          resolve();
+        }
+      )
+    })
+    return promesa;
 
-    this.http.get('https://webangular-c5058.firebaseio.com/productos_idx.json').subscribe(
-         res =>{
-                //setTimeout(()=>{
-                //console.log(res.json());
-                  this.productos = res.json();
-                  this.cargando = false;
+  }
 
-                //},1500)
-              }
-            )
-    }
-
-  public cargar_producto(cod:string){
+  public cargar_producto(cod: string) {
     return this.http.get(`https://webangular-c5058.firebaseio.com/productos/${cod}.json`)
+  }
+
+
+
+  public buscar_producto(termino: string) {
+    // console.log("Buscar Producto");
+    // console.log(this.productos.length);
+
+    if (this.productos.length === 0) {
+      this.cargar_productos().then(() => {
+        // Aqui me fijo si termino la carga
+        this.filtrar_productos(termino);
+      });
+    } else {
+      this.filtrar_productos(termino);
+    }
+  }
+
+  private filtrar_productos(termino: string) {
+    this.productos_filtrados = [];
+    termino = termino.toLowerCase();
+
+    this.productos.forEach(prod => {
+      if (prod.categoria.indexOf(termino) >= 0 || prod.titulo.toLowerCase().indexOf(termino) >= 0) {
+        this.productos_filtrados.push(prod);
+        //console.log(prod);
+      }
+      //console.log(prod);
+    })
+
+
+
   }
 
 }
